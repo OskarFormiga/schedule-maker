31cf75ab52a16a4b5f2ed2b28ca7cc8ab59e6be1
OskarFormiga <oscar.formiga@estudiantat.upc.edu>
2024-10-26 14:55:45 +0200

    asdf

--- planner/vite.config.js
+++ planner/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: './build',
-    chunkSizeWarningLimit: 1600,
-  },
-  base: '/schedule-maker/',
+
 })
